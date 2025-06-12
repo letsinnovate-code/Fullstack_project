62a8a9f5f971ebdc7c2b1c174db034fbfc7ffaf8
Anil Patel <panil9153@gmail.com>
2025-06-12 12:15:02 +0530

    hashing password using bcryptjs

--- model/User.model.js
+++ model/User.model.js
@@ -32,7 +32,12 @@ const userSchema = new mongoose.Schema({
     timestamps:true
 }
 )
-
+userSchema.pre("save", async function (next) {
+if(this.isModified("password")){
+    this.password = await bcrypt.hash(this.password, 10)
+}
+    next()
+})
 
 
  const User = mongoose.model("User", userSchema)
